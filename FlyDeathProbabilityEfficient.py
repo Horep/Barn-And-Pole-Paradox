a524f80424d6d38819f361ff528b24ef511af7e8
Horep <horep245@gmail.com>
2021-07-06 13:04:47 +0100

    Add files via upload

--- FlyDeathProbabilityEfficient.py
+++ FlyDeathProbabilityEfficient.py
@@ -1,6 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
-N = 100000000
+plot_histogram = 0
+N = 5*100**4
 
 
 def W(b, r):
@@ -17,18 +17,25 @@ def W(b, r):
 b_array = np.random.uniform(0, 1, N)
 g_array = 1/np.sqrt(1 - b_array*b_array)
 r_array = np.random.uniform(0, 1, N)*g_array
-W_array = W(b_array, r_array)/(r_array)
 
+g_array = None
+
+W_array = W(b_array, r_array)/(r_array)
+b_array = None
+r_array = None
 print(np.average(W_array))
 print(np.std(W_array))
-plt.grid(linewidth=0.2)
-plt.hist(W_array, bins=20, density=True, edgecolor='black', linewidth=1.2)
-plt.xlim(0, 1)
-plt.xticks(np.linspace(0, 1, 10+1))
-plt.xlabel(r"$W(\beta_{p,n},r_{n})/r_{n}$")
-plt.ylabel(r"Frequency")
-plt.yticks(np.linspace(0, 10, 10+1))
 
-plt.title(r"$N=$"+f"{N:.0e}")
-plt.savefig("FlyDeathHistogram.pdf", dpi=1000,
-            bbox_inches='tight', pad_inches=0.0)
+if plot_histogram == 1:
+    import matplotlib.pyplot as plt
+    plt.grid(linewidth=0.2)
+    plt.hist(W_array, bins=20, density=True, edgecolor='black', linewidth=1.2)
+    plt.xlim(0, 1)
+    plt.xticks(np.linspace(0, 1, 10+1))
+    plt.xlabel(r"$W(\beta_{p,n},r_{n})/r_{n}$")
+    plt.ylabel(r"Frequency")
+    plt.yticks(np.linspace(0, 10, 10+1))
+
+    plt.title(r"$N=$"+f"{N:.0e}")
+    plt.savefig("FlyDeathHistogram.pdf", dpi=1000,
+                bbox_inches='tight', pad_inches=0.0)
